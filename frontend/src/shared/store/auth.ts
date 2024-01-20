f38f1a85f8e832b620ba13570d1d553f0267f8f9
thrmdyy <thrmdy@gmail.com>
2024-01-20 23:31:14 +0500

    Update auth.ts

--- frontend/src/shared/store/auth.ts
+++ frontend/src/shared/store/auth.ts
@@ -1,20 +1,25 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
-import { immer } from "zustand/middleware/immer";
+import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+import { immer } from 'zustand/middleware/immer'
 
 interface IAuthStore {
-  isLoggedIn: boolean;
-  setIsLoggedIn: (payload: boolean) => void;
+  isLoggedIn: boolean
+  setIsLoggedIn: (payload: boolean) => void
 }
 
 export const useAuthStore = create<IAuthStore>()(
   devtools(
     immer((set) => ({
-      isLoggedIn: !!window.localStorage.getItem("jwt"),
+      isLoggedIn: !!window.localStorage.getItem('jwt'),
       setIsLoggedIn: (payload) =>
         set((state) => {
-          state.isLoggedIn = payload;
+          state.isLoggedIn = payload
         }),
-    }))
-  )
-);
+    })),
+  ),
+)
+
+const test1: Record<string, Array<string | number>> = { ['string']: [1, '2'] }
+const test2: Record<string, Record<string, Array<number | string>>> = {
+  ['string']: { ['string']: [1, '2'] },
+}
