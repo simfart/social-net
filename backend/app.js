e48b8bfe07a140da49fbeacfac5b8141206dceef
Zina Artyugina <simfart@ya.ru>
2024-02-21 13:45:31 +0300

    cors errors

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const { errors } = require('celebrate');
-const cors = require('cors');
-const helmet = require('helmet');
-const router = require('./routes/index');
-const errorHandler = require('./middlewares/errorHandler');
-const limiter = require('./middlewares/rateLimit');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { PORT, MONGO_ADRESS } = require('./utils/config');
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const { errors } = require('celebrate')
+const cors = require('cors')
+const helmet = require('helmet')
+const router = require('./routes/index')
+const errorHandler = require('./middlewares/errorHandler')
+const limiter = require('./middlewares/rateLimit')
+const { requestLogger, errorLogger } = require('./middlewares/logger')
+const { PORT, MONGO_ADRESS } = require('./utils/config')
 
-const app = express();
+const app = express()
 // mongoose.connect('mongodb+srv://artzina7:S1fZRT1rOGuFwpIi@cluster0.hpavfbe.mongodb.net/');
 mongoose.connect(MONGO_ADRESS)
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://127.0.0.1:27017',
-  ],
-  method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  credentials: true,
+app.use(
+  cors({
+    origin: [
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://127.0.0.1:27017',
+    ],
+    method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  }),
+)
 
-}));
+app.use(requestLogger)
 
-
-app.use(requestLogger);
-
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 app.use('/', router)
-app.use(limiter);
-app.use(helmet());
+app.use(limiter)
+app.use(helmet())
 
-
-
-app.use(errorLogger);
-app.use(errors());
-app.use(errorHandler);
-app.listen(PORT);
+app.use(errorLogger)
+app.use(errors())
+app.use(errorHandler)
+app.listen(PORT)
